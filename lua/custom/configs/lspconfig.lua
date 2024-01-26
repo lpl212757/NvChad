2610b4082aa0cd502d3a88777cb8235f8790660b
Le Phat Loc <lpl212757@gmail.com>
2024-01-26 22:56:39 +0700

    MOD - Remove unused comment

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -13,9 +13,6 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- 
--- lspconfig.pyright.setup { blabla}
---
 -- User load_config
 local pid = vim.fn.getpid()
 local util = require "lspconfig/util"
@@ -32,29 +29,4 @@ lspconfig.omnisharp.setup {
   root_dir = util.root_pattern("*.csproj", "*.sln"),
 }
 
--- lspconfig.angularls.setup({
---   root_dir = util.root_pattern("angular.json"),
---   single_file_support = false
--- })
-
 lspconfig.pylsp.setup({})
-
--- require("sonarlint").setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
---   server = {
---     cmd = {
---       "C:\\Users\\Admin\\AppData\\Local\\nvim-data\\mason\\packages\\sonarlint-language-server\\sonarlint-language-server.cmd",
---       "-stdio",
---       "-analyzers",
---        "C:\\Users\\Admin\\AppData\\Local\\nvim-data\\mason\\share\\sonarlint-analyzers\\sonarpython.jar",
---        "C:\\Users\\Admin\\AppData\\Local\\nvim-data\\mason\\share\\sonarlint-analyzers\\sonarlintomnisharp.jar"
---     },
---
---   },
---   filetypes = {
---     'python',
---     'cs'
---   },
--- }
--- -- End user config
