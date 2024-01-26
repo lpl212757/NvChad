8fda73b55e30b865afc4c6002518743e842ec706
Le Phat Loc <lpl212757@gmail.com>
2024-01-26 18:38:32 +0700

    MOD - Add custom lsp

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd" }
+local servers = { }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -13,5 +13,20 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- 
--- lspconfig.pyright.setup { blabla}
+-- User load_config
+local pid = vim.fn.getpid()
+local util = require "lspconfig/util"
+
+lspconfig.omnisharp.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {
+    "C:\\Users\\Admin\\AppData\\Local\\nvim-data\\mason\\packages\\omnisharp\\omnisharp",
+    "--languageserver",
+    "--hostPID",
+    tostring(pid),
+  },
+  root_dir = util.root_pattern("*.sln", "*.csproj"),
+}
+
+lspconfig.pylsp.setup({})
