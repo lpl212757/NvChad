349e2810171a379c36ae65cf5d875c9b40cfadcd
Loc Le <lpl212757@gmail.com>
2024-03-13 02:39:20 +0700

    feat - add config for neovide

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -21,6 +21,19 @@ vim.diagnostic.config({
   signs = true,
   float = { border = "single" },
 })
+vim.o.guifont = "SauceCodePro Nerd Font:h11" -- text below applies for Vimscript
+if vim.g.neovide then
+  vim.keymap.set('n', '<C-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<C-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<C-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<C-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap('', '<C-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<C-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<C-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<C-v>', '<C-R>+', { noremap = true, silent = true})
 -- End user config
 
 -------------------------------------- options ------------------------------------------
